be0ca0ec82af01f42aa2cff0c05fa62649dbec82
BrianHo <Curry30@Brians-MacBook-Pro.local>
2020-10-24 23:57:54 -0700

    basic template with footer and navbar components

---
+++ client/src/components/MainContent.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="Main">
+        <h2>Main Content</h2>
+    </div>
+</template>
---
+++ server.js
@@ -0,0 +1,5 @@
+const xp = require("express");
+const bp = require("body-parser");
+const router = require("./server/routes.js");
+const path = require("path");
+const app = xp();
