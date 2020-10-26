91bcc0255f72a17c958ccdb98188f7741fd1d641
BrianHo <Curry30@Brians-MacBook-Pro.local>
2020-10-25 20:55:23 -0700

    edit Navbar component, add new vue, create page route file

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -11,28 +11,25 @@
       <v-toolbar-title>Ho's space</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>Home</v-tab>
+          <v-tab>Seattle</v-tab>
+          <v-tab>YellowStone</v-tab>
+          <v-tab>California</v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{ 
+      items: [
+          'web', 'shopping', 'videos', 'images', 'news',
+        ],
+    }
+  }
+}
+</script>
